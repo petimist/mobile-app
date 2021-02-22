9cb69426e61ab0a7108ca94f864de9cfdcb85784
RecursivePerson <possawat2017@hotmail.com>
2021-02-22 21:40:34 +0700

    log in log out

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-native-paper';
 import App from './src';
 import { theme } from './src/core/theme';
+import firebase from './src/plugins/firebase'
 
 const Main = () => (
   <Provider theme={theme}>
