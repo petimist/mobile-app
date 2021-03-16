7bfbb5a2d6c04840c469cdec43aef0805176db44
parmcoder <possawat2017@gmail.com>
2021-03-16 17:13:13 +0700

    working on expo start, right now it does not render

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import { registerRootComponent } from 'expo';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
+import App from './src/App';
 
-import App from './App';
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => Main);
