ec85c816d35d14e91f7d06a846c85dacbb94e103
parmcoder <possawat2017@gmail.com>
2021-04-01 02:34:17 +0700

    Not working yet

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,21 +1,20 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { APIKEY, AUTHDOMAIN, PROJECTID} from 'react-native-dotenv'
+import config from '../../config.js';
 // require('dotenv').config();
+// import 'dotenv/config';
 
 // Initialize Firebase
 const firebaseConfig = {
-  // apiKey: "AIzaSyCXn2U-MVtTL5HveA1c5OwjfXt3Wl3FdD0",
-  //   authDomain: "petimist-2021.firebaseapp.com",
-  //   projectId: "petimist-2021"
-  apiKey: APIKEY,
-  authDomain: AUTHDOMAIN,
-  projectId: PROJECTID,
-  // storageBucket: process.env.STORAGEBUCKET,
-  // messagingSenderId: process.env.MESSAGINGSENDERID,
-  // appId: process.env.APPID,
-  // measurementId: process.env.MEASUREMENTID,
+  apiKey: 'AIzaSyCXn2U-MVtTL5HveA1c5OwjfXt3Wl3FdD0',
+  authDomain: 'petimist-2021.firebaseapp.com',
+  databaseURL: 'https://petimist-2021-default-rtdb.firebaseio.com',
+  projectId: 'petimist-2021',
+  storageBucket: 'petimist-2021.appspot.com',
+  messagingSenderId: '593747444736',
+  appId: '1:593747444736:web:bbb01d9d916bb25a18c4e5',
+  measurementId: 'G-874BSFE9N9',
 };
 
 // Your web app's Firebase configuration
@@ -24,12 +23,13 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-firebase.getCurrentUser = () => new Promise((resolve, reject) => {
-  const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-    unsubscribe();
-    resolve(user);
-  }, reject);
-});
+firebase.getCurrentUser = () =>
+  new Promise((resolve, reject) => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  });
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     console.log('logged in', user);
